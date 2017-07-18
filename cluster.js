49d7eed5deff8a2538510263c40d014f5efe75d7
sezanzeb <to.213692@protonmail.ch>
2017-07-18 13:28:45 +0200

    Add files via upload

---
+++ cluster.js
@@ -0,0 +1,15 @@
+var cluster = require('cluster')
+//var numCPUs = require('os').cpus().length
+var numCPUs = 4
+if (cluster.isMaster)
+{
+    console.log("starting 4 processes")
+    for (var i = 0; i < numCPUs; i++)
+    {
+        cluster.fork()
+    }
+}
+else
+{
+    require("./server.js")
+}
