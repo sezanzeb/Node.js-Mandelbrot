13b262bedbe413a0ef89a57b6d1f122c1e08c22d
sezanzeb <to.213692@protonmail.ch>
2017-09-29 01:10:29 +0200

    Delete asdf.js

--- asdf.js
+++
@@ -1,5 +0,0 @@
-var asdf = "data:{\"image\":\"\",\"requestcount\":0,\"allPointsB\":[[9.814995547304305,2.7220816326530404,1]]}"
-var asdf = asdf.split("data:")[1]
-var parsed = JSON.parse(asdf)
-
-console.log(parsed)
